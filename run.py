bde614ea2791091ecbae2391d142c46e7fa4371f
kiksgold <kiksgold@gmail.com>
2022-12-13 09:55:57 +0000

    Create Hidden & Guess Board for holding ship locations and displaying hits and miss

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# Board for holding ship locations
+HIDDEN_BOARD = [[''] * 8 for x in range(8)]
+# Board for displaying hits and miss
+GUESS_BOARD = [[''] * 8 for x in range(8)]
+
+let_to_num = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
