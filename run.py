9da2a05b5ccd83628bb9536742eac2197dea8b01
kiksgold <kiksgold@gmail.com>
2022-12-13 10:03:49 +0000

    Create Functions for our Battleship Game Board

--- run.py
+++ run.py
@@ -6,3 +6,22 @@ HIDDEN_BOARD = [[''] * 8 for x in range(8)]
 GUESS_BOARD = [[''] * 8 for x in range(8)]
 
 let_to_num = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
+
+# Define Function to Print Battleship Board
+def board():
+    pass
+
+
+# Define Function to create the ships
+def create_ships(board):
+    pass
+
+
+# Define Function to get ship location
+def get_ship_location():
+    pass
+
+
+# Define Function to count hit ships
+def count_hit_ships():
+    pass
