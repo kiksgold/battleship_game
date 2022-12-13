b28e38dd444b06719cc0d7fdcad82783af507416
kiksgold <kiksgold@gmail.com>
2022-12-13 10:13:50 +0000

    Define function to print battleship board

--- run.py
+++ run.py
@@ -8,8 +8,13 @@ GUESS_BOARD = [[''] * 8 for x in range(8)]
 let_to_num = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7}
 
 # Define Function to Print Battleship Board
-def board():
-    pass
+def print_board(board):
+    print(' A B C D E F G H')
+    print(' ---------------')
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
 
 
 # Define Function to create the ships
